2d26afdcc978be62eb1dcc5dfac24f6e08b73bcb
Linuz Tri Erianto <linuztri@apps.ipb.ac.id>
2022-11-30 19:57:51 +0700

    fixing

--- api/notes/coba.py
+++
@@ -1,5 +0,0 @@
-import pdfkit
-
-config = pdfkit.configuration(wkhtmltopdf = "/usr/bin/wkhtmltopdf")
-pdfkit.from_url("https://google.com", "test/test.pdf", verbose=True, configuration = config)
-print("="*50)
